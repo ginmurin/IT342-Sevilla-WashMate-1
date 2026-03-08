1979536d618f90e09310375b6bec15fa40f01f37
Ginmurin <johnemmanuel.sevilla@cit.edu>
2026-03-09 07:44:25 +0800

    Springboot initialization

---
+++ washmate/src/main/java/edu/cit/sevilla/washmate/WashmateApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.sevilla.washmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashmateApplication.class, args);
+	}
+
+}
---
+++ washmate/src/test/java/edu/cit/sevilla/washmate/WashmateApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.sevilla.washmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
